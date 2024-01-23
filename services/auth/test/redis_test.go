a61d687fb7ff9f2045bd94b4a8c0333c83ce92a7
Aditya Suryadi <adityasuryaditya@gmail.com>
2024-01-23 18:46:54 +0700

    add refresh token

---
+++ services/auth/test/redis_test.go
@@ -0,0 +1,20 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/redis/go-redis/v9"
+	"github.com/stretchr/testify/assert"
+)
+
+var client = redis.NewClient(&redis.Options{
+	Addr: "localhost:6379",
+	DB:   0,
+})
+
+func TestConnection(t *testing.T) {
+	assert.NotNil(t, client)
+
+	err := client.Close()
+	assert.Nil(t, err)
+}
